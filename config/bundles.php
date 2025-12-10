00bd8638a6a7e1cac25d3525dd062aa1e25ae599
Pavel <kutepov_pasha@list.ru>
2025-12-10 14:49:26 +0500

    updated security

--- config/bundles.php
+++ config/bundles.php
@@ -3,5 +3,4 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
